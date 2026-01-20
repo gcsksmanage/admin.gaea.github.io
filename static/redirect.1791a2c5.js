c44f258431ed891bd96361a7b181108dda33fdab
gaea3 <develop@aigaea.net>
2026-01-20 03:07:34 +0000

    update 2026-01-20 03:07:33

---
+++ static/redirect.1791a2c5.js
@@ -0,0 +1,1 @@
+import{c as e,o as r}from"./entry.89aa1c3d.js";const _={__name:"redirect",setup(t){return(c,o)=>(r(),e("div"))}};export{_ as default};
---
+++ static/router-login.d032ce5a.js
@@ -0,0 +1,1 @@
+import{ai as t,U as a,ah as e}from"./entry.89aa1c3d.js";const n=t((r,o)=>{if(a().getToken){if(r.path==="/")return e("/task-audit")}else return e("/login")});export{n as default};
