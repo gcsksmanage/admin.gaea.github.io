982d6d2245f7fbd3e25c2cb56c3806a3dce27eeb
gaea3 <develop@aigaea.net>
2026-01-20 01:38:32 +0000

    merge

---
+++ static/redirect.a0e8613a.js
@@ -0,0 +1,1 @@
+import{aq as a,U as r,a7 as s}from"./entry.15a76552.js";const u=a((o,e)=>{const t=r();if(e.query.token)return t.setToken(e.query.token),s(`/${e.query.path}`)});export{u as default};
---
+++ static/router-login.3642730b.js
@@ -0,0 +1,1 @@
+import{aq as t,U as a,a7 as e}from"./entry.15a76552.js";const n=t((r,o)=>{if(a().getToken){if(r.path==="/")return e("/task-audit")}else return e("/login")});export{n as default};
